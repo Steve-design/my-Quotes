b5bf33990b43a1e21d011c1439156406a150d1e7
Steve-design <smzalendo31@gmail.com>
2019-09-20 17:02:26 +0300

    add new property quote to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'Energy is precious, use your energy to build not to destroy';
 }
