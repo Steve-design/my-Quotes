844ea16708978f647562806195c9b21ad106856b
Steve-design <smzalendo31@gmail.com>
2019-09-21 12:00:44 +0300

    define the array of quotes inside the QuoteCompenent definition class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote = [
-    {id:1, name:'Energy is precious, use your energy to build not to destroy'},
-    {id:2,name:'The sensation of energy expands with increasing relaxation'},
-    {id:3,name:'The more you lose yourself in something bigger than yourself, the more energy you will have'},
-    {id:4,name:'A person of intellect without energy added to it, is a failure'},
-    {id:5,name:'Energy and persistence alter all things'},
-    {id:6,name:"Nearly all men can stand adversity, but if you want to test a man's character, give him power"},
-    {id:7,name:'I wonder if fears ever really go away, or if they just lose their power over us'},
-  ];
+ 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,7 +6,15 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes:Quote = [
+    {id:1, name:'Energy is precious, use your energy to build not to destroy'},
+    {id:2,name:'The sensation of energy expands with increasing relaxation'},
+    {id:3,name:'The more you lose yourself in something bigger than yourself, the more energy you will have'},
+    {id:4,name:'A person of intellect without energy added to it, is a failure'},
+    {id:5,name:'Energy and persistence alter all things'},
+    {id:6,name:"Nearly all men can stand adversity, but if you want to test a man's character, give him power"},
+    {id:7,name:'I wonder if fears ever really go away, or if they just lose their power over us'},
+  ];
   constructor() { }
 
   ngOnInit() {
