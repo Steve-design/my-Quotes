2a6cbed2fd844fd70489fdbe33caf4f8da6aa164
Steve-design <smzalendo31@gmail.com>
2019-09-21 10:48:44 +0300

    create a constructor function and give it an array of three strings value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'Energy is precious, use your energy to build not to destroy';
+  quotes:string[];
+
+  constructor(){
+    this.quotes = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+  } 
 }
