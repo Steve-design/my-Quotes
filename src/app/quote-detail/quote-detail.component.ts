9c46079031aa90c7808fc79468f27dbcf34c0cc3
Steve-design <smzalendo31@gmail.com>
2019-09-21 13:04:02 +0300

    import Quote to quote-detail component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
