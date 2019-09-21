5273ba5ffe4b090f2c00f967e8d818c7aa6ddf1f
Steve-design <smzalendo31@gmail.com>
2019-09-21 11:13:02 +0300

    create quotes using the quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-
-  constructor(){
-    this.quotes = ['Energy is precious, use your energy to build not to destroy', 'The sensation of energy expands with increasing relaxation', 'The more you lose yourself in something bigger than yourself, the more energy you will have']
-  } 
+  quotes:Quote = [
+    {id:1, name:'Energy is precious, use your energy to build not to destroy'},
+    {id:2,name:'The sensation of energy expands with increasing relaxation'},
+    {id:3,name:'The more you lose yourself in something bigger than yourself, the more energy you will have'},
+    {id:4,name:'A person of intellect without energy added to it, is a failure'},
+    {id:5,name:'Energy and persistence alter all things'},
+    {id:6,name:"Nearly all men can stand adversity, but if you want to test a man's character, give him power"},
+    {id:7,name:'I wonder if fears ever really go away, or if they just lose their power over us'},
+  ];
 }
